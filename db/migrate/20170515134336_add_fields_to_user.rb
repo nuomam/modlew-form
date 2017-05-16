aa24a5d0b761c07fe4afc73853b87de7476bec38
Mamoun Benmeradi <mamoun@benmeradi.com>
2017-05-16 16:07:44 +0200

    update

--- db/migrate/20170515134336_add_fields_to_user.rb
+++ db/migrate/20170515134336_add_fields_to_user.rb
@@ -1,13 +1,9 @@
 class AddFieldsToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :general_diy, :boolean, default: false
-    add_column :users, :electricity, :boolean, default: false
-    add_column :users, :plumbing, :boolean, default: false
-    add_column :users, :building, :boolean, default: false
-    add_column :users, :gardening, :boolean, default: false
-    add_column :users, :decoration, :boolean, default: false
     add_column :users, :weldom_tester, :boolean
     add_column :users, :construction_type, :string
     add_column :users, :construction_surface, :integer
+    add_column :users, :construction_color, :string
+    add_column :users, :card_number, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,18 +31,18 @@ ActiveRecord::Schema.define(version: 20170515141123) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
     t.text     "address"
@@ -56,15 +56,11 @@ ActiveRecord::Schema.define(version: 20170515141123) do
     t.string   "youtube_channel"
     t.string   "personnal_blog"
     t.text     "motivation"
-    t.boolean  "general_diy",            default: false
-    t.boolean  "electricity",            default: false
-    t.boolean  "plumbing",               default: false
-    t.boolean  "building",               default: false
-    t.boolean  "gardening",              default: false
-    t.boolean  "decoration",             default: false
     t.boolean  "weldom_tester"
     t.string   "construction_type"
     t.integer  "construction_surface"
+    t.string   "construction_color"
+    t.string   "card_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
