e087f790c36f9ae33b4c7da3aba5b1c11286f7bd
Mamoun Benmeradi <mamoun@benmeradi.com>
2017-05-17 15:03:15 +0200

    Feedback + seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address , presence: true
   validates :postal_code, presence: true, format: { with: /(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}/ }
   validates :city, presence: true
   validates :policy_agreement, presence: true
+  validates :phone_number, presence: true
   # validates :construction_type, inclusion: { in: ["Repeindre un meuble de cuisine", "Repeindre un escalier", "Repeindre un carrelage de salle de bain"] }
   has_attachment :photo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515141123) do
+ActiveRecord::Schema.define(version: 20170517124512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20170515141123) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,6 +67,7 @@ ActiveRecord::Schema.define(version: 20170515141123) do
     t.integer  "construction_surface"
     t.string   "construction_color"
     t.string   "card_number"
+    t.string   "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
