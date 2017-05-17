bbd9d2674b654dc0698e33e11bae8fb5d0bc9ad6
Mamoun Benmeradi <mamoun@benmeradi.com>
2017-05-17 15:59:22 +0200

    delete admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :registration_steps
   get '/confirmation/', to: 'pages#confirmation', as: 'confirmation'
-  get "/admin", to: 'admin/admins#index'
-  namespace :admin do
-    resources :users
-  end
+  # get "/admin", to: 'admin/admins#index'
+  # namespace :admin do
+  #   resources :users
+  # end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
