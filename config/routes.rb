6c7a7eb685e13f01e7095e9e785d53e7f2ea54e7
Mamoun Benmeradi <mamoun@benmeradi.com>
2017-07-06 19:13:26 +0200

    Redirect to Weldom.com

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get "/" => redirect("https://www.weldom.fr/")
   mount Attachinary::Engine => "/attachinary"
   devise_for :users, controllers: {:registrations => "users/registrations"}
-  root to: 'pages#home'
-  resources :registration_steps
-  get '/confirmation/', to: 'pages#confirmation', as: 'confirmation'
-  get '/export_22wibi/', to: 'pages#export_csv', as: 'export_csv'
+  # root to: 'pages#home'
+  # resources :registration_steps
+  # get '/confirmation/', to: 'pages#confirmation', as: 'confirmation'
+  # get '/export_22wibi/', to: 'pages#export_csv', as: 'export_csv'
   # get "/admin", to: 'admin/admins#index'
   # namespace :admin do
   #   resources :users
